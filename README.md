df6034e9ea8e817892616ad8311f5e0caef4305e
Tanvir Ather <tanvir.ather@zuhid.com>
2025-01-09 19:11:13 -0700

    add fake dart server

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mock_server_dart
-a mock server written in dart
+# crowdilm-app-flutter
+CrowdIlm appliation written in flutter
+
+
+# Setup Development Environment
+- crete a ssh key
+- login to github and upload pubic key in "https://github.com/settings/keys"
+- `git clone git@github.com:zuhid/mock_server_dart` to clone the project
+- `dart pub get` to install missing packages
+- `dart run server.dart` to run the the app
+
---
+++ server.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+// Configure routes.
+final _router = Router()
+  ..get('/', (req) => Response.ok('Server is running'))
+  ..all('/<.*>', _allHandler);
+
+Future<Response> _allHandler(Request request) async {
+  var accept = request.headers["Accept"] ?? 'application/json';
+  var (extension, contentType) = switch (accept) {
+    'text/csv' => ('csv', accept),
+    'text/html' => ('html', accept),
+    _ => ('json', 'application/json'),
+  };
+
+  var filePath = '${Directory.current.path}/files/${request.url.path}.$extension';
+  var file = File(filePath);
+
+  if (await file.exists()) {
+    var fileContent = await file.readAsString();
+    return Response.ok(fileContent, headers: {'content-type': contentType});
+  }
+  return Response.notFound('${request.url.path} could not find $filePath');
+}
+
+void main(List<String> args) async {
+  final server = await serve(
+      Pipeline().addMiddleware(logRequests()).addHandler(_router.call), // Configure a pipeline that logs requests.
+      InternetAddress.anyIPv4, // Use any available host or container IP (usually `0.0.0.0`).
+      8081);
+  print('Server listening on http://localhost:${server.port}');
+}
